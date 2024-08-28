61482d100e911721e1e83622a6a3c22fb8c559ba
Gokul-anand-29 <gokulanand200@gmail.com>
2024-08-28 14:33:42 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  float temp_min = 0;
-  float temp_max = 45;
-  float soc_min = 20;
-  float soc_max = 80;
-  float charge = 0.8;  
-  if (temperature < temp_min || temperature > temp_max || soc < soc_min || soc > soc_max || chargeRate > charge) {
+int batteryIsOk(float temperature, float soc, float chargeRate) { 
+  
+  if (temperature < 0 || temperature > 45 || soc < 20 || soc > 80 || chargeRate > 0.8) {
         printf("Battery parameters out of range!\n");
         return 0;
-        break;
     }
-
-    return 1;
+  else {
+      return 1;
+    }
 }
 
 int main() {

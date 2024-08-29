a89ff269079bd3b704f89b279d946641c0953e92
Gokul-anand-29 <gokulanand200@gmail.com>
2024-08-29 17:33:56 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -45,7 +45,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 int main() {
     assert(batteryIsOk(25, 70, 0.7));//no print
     assert(batteryIsOk(55, 50, 0.2));//temp out of range
-    assert(batteryIsOk(-1, 85, 0.0));//temp out of range
+    assert(batteryIsOk(-1, 75, 0.0));//temp out of range
     assert(batteryIsOk(20, 90, 0.2));//Soc Out of range
     assert(batteryIsOk(20, 15, 0.2));//Soc Out of range
     assert(batteryIsOk(25, 25, 0.8));//charge out of range
